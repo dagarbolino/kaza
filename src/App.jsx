10b2c56c368876bced93f059e13dd8667f4c3acc
alexandre dnk <dincq.alexandre@gmail.com>
2024-01-18 01:40:18 +0100

    Add react-router-dom and update dependencies and scss ok

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+import React from "react";
+import MainNavigation from "./components/Ui/MainNavigation";
+
+function App() {
+  return (
+    <>
+      <MainNavigation />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Ui/MainNavigation.jsx
@@ -0,0 +1,26 @@
+const MainNavigation = () => {
+  return (
+    <>
+      <nav className="containerNavbar">
+        <div className="logo">
+          <img src="#" alt="#" />
+        </div>
+
+        <div className="navLinks">
+          <navLinks>
+            <ul>
+              <li>
+                <a href="#">Acceuil</a>
+              </li>
+              <li>
+                <a href="#">A Propos</a>
+              </li>
+            </ul>
+          </navLinks>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default MainNavigation;
